ba9b7cc9e35c3a42c68123129e46afaf5b8644fa
renan <renan.rodriguesmoura2022@gmail.com>
2022-10-27 23:49:27 -0300

    adicionando elementos e atualizando main js

--- mian.js
+++ mian.js
@@ -1,10 +1,12 @@
 const addCarrinho = document.querySelectorAll('.botao');
 const popup = document.querySelector('.popup-wrapper');
 const closeBtn = document.querySelector('.popup-close');
+const qtn = document.getElementById('quantidade')
 
 addCarrinho.forEach((itemAtual) => {
     itemAtual.addEventListener('click', () => {
         popup.style.display = 'block';
+        qtn.focus();
     })
 })
 
