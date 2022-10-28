ad87c064c4ffe106d5a21a65c90214f91f40e36f
renan <renan.rodriguesmoura2022@gmail.com>
2022-10-28 19:46:51 -0300

    modificando main js

--- mian.js
+++ mian.js
@@ -1,15 +1,31 @@
 const addCarrinho = document.querySelectorAll('.botao');
 const popup = document.querySelector('.popup-wrapper');
 const closeBtn = document.querySelector('.popup-close');
-const qtn = document.getElementById('quantidade')
+const qtn = document.getElementById('quantidade');
+const botao = document.querySelectorAll('[data-botao]');
 
 addCarrinho.forEach((itemAtual) => {
     itemAtual.addEventListener('click', () => {
         popup.style.display = 'block';
-        qtn.focus();
     })
 })
 
 closeBtn.addEventListener('click', () => {
     popup.style.display = 'none';
 })
+
+botao.forEach((elemento) => {
+    elemento.addEventListener('click', (evento) => {
+        adcItem(evento.target.dataset.botao, evento.target.parentNode);
+    })
+})
+
+function adcItem (operacao, controle) {
+    const unidade = controle.querySelector('[data-unidade]');
+
+    if(operacao === '+'){
+        unidade.value = parseInt(unidade.value) + 1 + ' unidades';
+    }else{
+        unidade.value = parseInt(unidade.value) - 1 + ' unidades';
+    }
+}
